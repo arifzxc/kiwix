7a4612345dec9cee544d359d1456f69587144f8e
arifzxc <113068651+arifzxc@users.noreply.github.com>
2022-10-10 02:40:40 +0700

    Update kiwix.sh

--- kiwix.sh
+++ kiwix.sh
@@ -9,7 +9,6 @@ wget https://download.kiwix.org/release/kiwix-tools/kiwix-tools_linux-armhf-3.3.
 wget https://download.kiwix.org/zim/wikipedia/wikipedia_id_all_maxi_2022-05.zim
 wget https://download.kiwix.org/zim/wikipedia/wikipedia_id_all_nopic_2022-05.zim
 
-cd /usr/local/src
 tar zxvf kiwix-tools_linux-armhf-3.3.0-1.tar.gz
 mv kiwix-tools_linux-armhf-3.3.0-1/kiwix-* /usr/local/bin/
 chmod -Rf 777 /usr/local/bin
